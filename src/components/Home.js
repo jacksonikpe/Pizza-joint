33e89b595135a7b644eda216869cc26c7b507a5f
Jackson Collins-Ikpe <jacksoncikpe@gmail.com>
2022-02-19 15:53:25 +0100

    added animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,16 +4,25 @@ import { motion } from "framer-motion";
 
 const Home = () => {
   return (
-    <div className="home container">
-      <motion.h2 animate={{ fontSize: 50, color: "#ff2994" }}>
-        Welcome to Pizza Joint
-      </motion.h2>
+    <motion.div
+      className="home container"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 1.5, duration: 1.5 }}
+    >
+      <h2>Welcome to Pizza Joint</h2>
       <Link to="/base">
-        <motion.button animate={{ scale: 1.5 }}>
+        <motion.button
+          whileHover={{
+            scale: 1.1,
+            textShadow: "0px 0px 8px rgb(255,255,255)",
+            boxShadow: "0px 0px 7px rgb(255,255,255)",
+          }}
+        >
           Create Your Pizza
         </motion.button>
       </Link>
-    </div>
+    </motion.div>
   );
 };
 
