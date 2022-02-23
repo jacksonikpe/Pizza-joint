1c1fa2da5fd962f819bbf30ec3750063871384c0
Jackson Collins-Ikpe <jacksoncikpe@gmail.com>
2022-02-23 22:40:19 +0100

    made logo interactive

--- src/components/Header.js
+++ src/components/Header.js
@@ -27,7 +27,12 @@ const pathVariants = {
 const Header = () => {
   return (
     <header>
-      <div className="logo">
+      <motion.div
+        drag
+        dragConstraints={{ left: 0, top: 0, right: 0, bottom: 0 }}
+        dragElastic={1}
+        className="logo"
+      >
         <motion.svg
           className="pizza-svg"
           xmlns="http://www.w3.org/2000/svg"
@@ -47,7 +52,7 @@ const Header = () => {
             d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z"
           />
         </motion.svg>
-      </div>
+      </motion.div>
       <motion.div
         initial={{ y: -250 }}
         animate={{ y: -10 }}
